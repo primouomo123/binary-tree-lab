b12a8b05470a8740908a5eb790d4433ab66c2df9
Luis Rios Alvarado <luismanuelrios123@gmail.com>
2026-03-17 18:38:39 -0400

    Final solution

--- binary_tree_lab.py
+++ binary_tree_lab.py
@@ -8,8 +8,25 @@ class TreeNode:
 
 # TODO: Implement the max_depth function
 def max_depth(root: Optional[TreeNode]) -> int:
-    pass
+    if root is None:
+        return 0
+    else:
+        left_depth = max_depth(root.left)
+        right_depth = max_depth(root.right)
+        return max(left_depth, right_depth) + 1
 
 # TODO: Implement the lowest_common_ancestor function
 def lowest_common_ancestor(root: TreeNode, p: TreeNode, q: TreeNode) -> TreeNode:
-    pass
+    if root is None:
+        return None
+
+    if root == p or root == q:
+        return root
+
+    left_lca = lowest_common_ancestor(root.left, p, q)
+    right_lca = lowest_common_ancestor(root.right, p, q)
+
+    if left_lca and right_lca:
+        return root
+
+    return left_lca if left_lca is not None else right_lca
